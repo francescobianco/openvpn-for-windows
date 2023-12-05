dd5c45b46ddba4655c78b0d137f09c0ac4584bc1
Francesco Bianco <info.francescobianco@gmail.com>
2023-12-05 12:54:19 +0100

    Fix instruction

--- setup.sh
+++ setup.sh
@@ -32,6 +32,15 @@ cp pki/ca.crt "C:/Program Files/OpenVPN/config/"
 cp pki/dh.pem "C:/Program Files/OpenVPN/config/"
 cp pki/issued/server.crt "C:/Program Files/OpenVPN/config/"
 cp pki/private/server.key "C:/Program Files/OpenVPN/config/"
-cp "C:/Program Files/OpenVPN/sample-config/server.ovpn" "C:/Program Files/OpenVPN/config/"
+"C:/Program Files/OpenVPN/bin/openvpn.exe" --genkey tls-auth "C:/Program Files/OpenVPN/config/ta.key"
 
+cp "C:/Program Files/OpenVPN/sample-config/server.ovpn" "C:/Program Files/OpenVPN/config"
+mv "C:/Program Files/OpenVPN/config/server.ovpn" "C:/Program Files/OpenVPN/config/server.ovpn.0"
 
+./bin/sed -e 's/^ca .*/ca "C:\\Program Files\\OpenVPN\\config\\ca.crt"/g' "C:/Program Files/OpenVPN/config/server.ovpn.0" > "C:/Program Files/OpenVPN/config/server.ovpn.1"
+./bin/sed -e 's/^cert .*/cert "C:\\Program Files\\OpenVPN\\config\\server.crt"/g' "C:/Program Files/OpenVPN/config/server.ovpn.1" > "C:/Program Files/OpenVPN/config/server.ovpn.2"
+./bin/sed -e 's/^key .*/key "C:\\Program Files\\OpenVPN\\config\\server.key"/g' "C:/Program Files/OpenVPN/config/server.ovpn.2" > "C:/Program Files/OpenVPN/config/server.ovpn.3"
+./bin/sed -e 's/^dh .*/dh "C:\\Program Files\\OpenVPN\\config\\dh.pem"/g' "C:/Program Files/OpenVPN/config/server.ovpn.3" > "C:/Program Files/OpenVPN/config/server.ovpn.4"
+./bin/sed -e 's/^tls-auth .*/tls-auth "C:\\Program Files\\OpenVPN\\config\\ta.key"/g' "C:/Program Files/OpenVPN/config/server.ovpn.4" > "C:/Program Files/OpenVPN/config/server.ovpn.5"
+
+mv "C:/Program Files/OpenVPN/config/server.ovpn.5" "C:/Program Files/OpenVPN/config/server.ovpn"
